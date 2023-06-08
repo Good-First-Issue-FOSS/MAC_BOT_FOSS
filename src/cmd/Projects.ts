10b7d0635701ab852bed15e9787feaa883d88ac8
Naomi Carrigan <nhcarrigan@gmail.com>
2023-06-08 00:29:22 -0700

    feat: enable linter, add ci (#18)
    * feat: add typescript eslint plugins
    
    Necessary to get eslint to run on TypeScript files.
    
    * feat: add lint script
    
    Makes linting easier.
    
    * feat: fix linting issues
    
    * feat: add CI

--- src/cmd/Projects.ts
+++ src/cmd/Projects.ts
@@ -1,80 +1,106 @@
-//this is a test dierectry for testing all new slsh command before implementing in the actual folder
+// this is a test dierectry for testing all new slsh command before implementing in the actual folder
 
-import  { listProjectNames, listUsers } from '../data/MongoData'
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, GuildMember } from "discord.js";
-const { SlashCommandBuilder } = require('discord.js');
-const wait = require('node:timers/promises').setTimeout;
+import { listProjectNames } from '../data/MongoData';
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ChatInputCommandInteraction,
+	GuildMember,
+	SlashCommandBuilder,
+} from 'discord.js';
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('projects')
 		.setDescription('wanna work on any project?'),
-	async execute(interaction:ChatInputCommandInteraction) {
+	async execute(interaction: ChatInputCommandInteraction) {
 		const target = interaction.options.getUser('target');
-		const reason = interaction.options.getString('reason') ?? 'No reason provided';
+		const reason =
+			interaction.options.getString('reason') ?? 'No reason provided';
 
-        const projectsButtons: ButtonBuilder[] = [];
-        const dataButtons: string[] = await listProjectNames()
+		const projectsButtons: ButtonBuilder[] = [];
+		const dataButtons: string[] = await listProjectNames();
 
-        dataButtons.forEach(
-            ele=>{
-                projectsButtons.push(
-                    new ButtonBuilder()
-                    .setCustomId(ele)
-                    .setLabel(ele)
-                    .setStyle(ButtonStyle.Secondary)
-                )
-            }
-        )
+		dataButtons.forEach((ele) => {
+			projectsButtons.push(
+				new ButtonBuilder()
+					.setCustomId(ele)
+					.setLabel(ele)
+					.setStyle(ButtonStyle.Secondary),
+			);
+		});
 
-		projectsButtons.push(new ButtonBuilder()
-			.setCustomId('cancel')
-			.setLabel('Cancel')
-			.setStyle(ButtonStyle.Danger))
+		projectsButtons.push(
+			new ButtonBuilder()
+				.setCustomId('cancel')
+				.setLabel('Cancel')
+				.setStyle(ButtonStyle.Danger),
+		);
 
-		const row = new ActionRowBuilder<ButtonBuilder>()
-			.addComponents(...projectsButtons);
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+			...projectsButtons,
+		);
 
 		const response = await interaction.reply({
 			content: `Are you sure you want to ban ${target} for reason: ${reason}?`,
 			components: [row],
 		});
 
-		const collectorFilter = (i: { user: { id: any; }; }) => i.user.id === interaction.user.id;
 		try {
-			const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
-		
+			const confirmation = await response.awaitMessageComponent({
+				filter: (i) => i.user.id === interaction.user.id,
+				time: 60_000,
+			});
+
 			if (confirmation.customId === 'cancel') {
-				//await interaction.guild.members.ban(target);
-                        await confirmation.update({ content: 'Action cancelled', components: [] });
+				// await interaction.guild.members.ban(target);
+				await confirmation.update({
+					content: 'Action cancelled',
+					components: [],
+				});
 			} else {
-                dataButtons.forEach(
-                    async ele=>{
-                        if(confirmation.customId === ele){
-                            await confirmation.update({ content: 'Granting Access', components: [] });
-    							const member = interaction.member as GuildMember;
+				dataButtons.forEach(async (ele) => {
+					if (confirmation.customId === ele) {
+						await confirmation.update({
+							content: 'Granting Access',
+							components: [],
+						});
+						const member = interaction.member as GuildMember;
 
-							    const role = interaction.guild?.roles.cache.find((r) => r.name === ele);
+						const role = interaction.guild?.roles.cache.find(
+							(r) => r.name === ele,
+						);
 
-    							if (role) {
-    							    try {
-    							        await member.roles.add(role);
-    							        await interaction.editReply({ content: 'Thanks for choosing ' + ele, components: [] });
-    							    } catch (error) {
-    							        console.error('Error adding role:', error);
-    							        await interaction.editReply({ content: 'An error occurred while adding the role.', components: [] });
-    							    }
-    							} else {
-    							    await interaction.editReply({ content: 'Role not found for ' + ele, components: [] });
-    							}
-                        }
-                    }
-                )
+						if (role) {
+							try {
+								await member.roles.add(role);
+								await interaction.editReply({
+									content: 'Thanks for choosing ' + ele,
+									components: [],
+								});
+							} catch (error) {
+								console.error('Error adding role:', error);
+								await interaction.editReply({
+									content: 'An error occurred while adding the role.',
+									components: [],
+								});
+							}
+						} else {
+							await interaction.editReply({
+								content: 'Role not found for ' + ele,
+								components: [],
+							});
+						}
+					}
+				});
 			}
-		}
-        catch (e) {
+		} catch (e) {
 			console.log(e);
-			await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+			await interaction.editReply({
+				content: 'Confirmation not received within 1 minute, cancelling',
+				components: [],
+			});
 		}
 	},
 };
--- src/utils/loadEnv.ts
+++ src/utils/loadEnv.ts
@@ -1,17 +1,17 @@
 export const loadEnv = () => {
-  if (
-    !process.env.TOKEN ||
-    !process.env.MONGO_URI ||
-    !process.env.APPLICATION_ID ||
-    !process.env.GUILD_ID
-  ) {
-    throw new Error("Missing environment variables.");
-  }
+	if (
+		!process.env.TOKEN ||
+		!process.env.MONGO_URI ||
+		!process.env.APPLICATION_ID ||
+		!process.env.GUILD_ID
+	) {
+		throw new Error('Missing environment variables.');
+	}
 
-  return {
-    TOKEN: process.env.TOKEN,
-    MONGO_URI: process.env.MONGO_URI,
-    APPLICATION_ID: process.env.APPLICATION_ID,
-    GUILD_ID: process.env.GUILD_ID,
-  };
+	return {
+		TOKEN: process.env.TOKEN,
+		MONGO_URI: process.env.MONGO_URI,
+		APPLICATION_ID: process.env.APPLICATION_ID,
+		GUILD_ID: process.env.GUILD_ID,
+	};
 };
