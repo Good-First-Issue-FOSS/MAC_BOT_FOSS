50a72cc0e835ec5e16d76606e19c434871396561
20B81A05D1 <20B81A05D1@gmail.com>
2023-06-09 19:52:18 +0530

    Bugs fixed on URL parameter

--- src/Server/index.ts
+++ src/Server/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { loadEnv } from '../utils/loadEnv';
 import axios from 'axios';
+import url from 'url';
 
 const app = express();
 const port = 53134;
@@ -11,28 +12,23 @@ app.get('/', async (request: Request, response: Response) => {
 	console.log(request.query);
 	const { code } = request.query;
 
-	if (!code) {
+	if (code) {
+		const formData = new url.URLSearchParams({
+			client_id: CLIENT_ID,
+			client_secret: CLIENT_SECRET,
+			grant_type: 'authorization_code',
+			code: code!.toString(),
+			redirect_uri: `http://localhost:${port}`,
+		});
 		const recivedToken = await axios.post(
 			'https://discord.com/api/v10/oauth2/token',
-			{
-				client_id: CLIENT_ID,
-				client_secret: CLIENT_SECRET,
-				grant_type: 'authorization_code',
-				code: code,
-				redirect_uri: `http://localhost:${port}`,
-			},
-			{
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-				},
-			},
+			formData.toString(),
 		);
-		console.log(recivedToken);
+		console.log(recivedToken.data);
 	} else {
 		console.log('There is no code');
 		response.sendStatus(400);
 	}
-	response.send(200);
 });
 
 app.listen(port, () =>
