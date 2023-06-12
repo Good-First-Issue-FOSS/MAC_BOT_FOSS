34fca3114b60754a773744eb4935816b34f01bd3
A. Athithya Ithayan <130672327+20B81A05D1@users.noreply.github.com>
2023-06-12 20:35:58 +0530

    fixed #3 in the Auth (#22)
    * fixed #20
    
    * Updated the auth command fixed #3

--- src/Server/index.ts
+++ src/Server/index.ts
@@ -3,6 +3,7 @@ import { loadEnv } from '../utils/loadEnv';
 import { UserData } from '../interfaces/UserData';
 import axios from 'axios';
 import url from 'url';
+import { addUsers } from '../data/MongoData';
 
 const app = express();
 const port = 53134;
@@ -10,127 +11,137 @@ const CLIENT_ID = loadEnv().APPLICATION_ID;
 const CLIENT_SECRET = loadEnv().CLIENT_SECRET;
 
 app.get('/', async (request: Request, response: Response) => {
-	console.log(request.query);
-	const { code } = request.query;
-	let flag = 0;
-	const problemArray: string[] = [];
-	// Initalizing storage data
-	const data: UserData = {
-		_id: '',
-		name: '',
-		refresh_token: '',
-		isEmailPresent: false,
-		email: null,
-		isGuthub: false,
-		github: {
-			type: '',
+	try {
+		console.log(request.query);
+		const { code } = request.query;
+		let flag = 0;
+		const problemArray: string[] = [];
+		// Initalizing storage data
+		const data: UserData = {
 			id: '',
 			name: '',
-			visibility: 0,
-			show_activity: false,
-			verified: false,
-			metadata_visibility: 0,
-		},
-	};
-	if (code) {
-		// getting initial token
-		const formData = new url.URLSearchParams({
-			client_id: CLIENT_ID,
-			client_secret: CLIENT_SECRET,
-			grant_type: 'authorization_code',
-			code: code?.toString(),
-			redirect_uri: `http://localhost:${port}`,
-		});
-		const recivedToken = await axios.post(
-			'https://discord.com/api/v10/oauth2/token',
-			formData.toString(),
-		);
-		data.refresh_token = recivedToken.data.refresh_token;
-		console.log(recivedToken.data);
-
-		// getting user's discord data
-		if (recivedToken.data.expires_in) {
-			const userData = await axios.get(
-				'https://discord.com/api/v10/users/@me',
-				{
-					headers: {
-						'Content-Type': 'application/json',
-						Authorization: `Bearer ${recivedToken.data.access_token}`,
-					},
-				},
+			refresh_token: '',
+			isEmailPresent: false,
+			email: null,
+			isGuthub: false,
+			github: {
+				type: '',
+				id: '',
+				name: '',
+				visibility: 0,
+				show_activity: false,
+				verified: false,
+				metadata_visibility: 0,
+			},
+		};
+		if (code) {
+			// getting initial token
+			const formData = new url.URLSearchParams({
+				client_id: CLIENT_ID,
+				client_secret: CLIENT_SECRET,
+				grant_type: 'authorization_code',
+				code: code?.toString(),
+				redirect_uri: `http://localhost:${port}`,
+			});
+			const recivedToken = await axios.post(
+				'https://discord.com/api/v10/oauth2/token',
+				formData.toString(),
 			);
-			console.log(userData.data);
-			if (userData.data.email) {
-				data._id = userData.data.id;
-				data.name = userData.data.username;
-				data.isEmailPresent = true;
-				data.email = userData.data.email;
+			data.refresh_token = recivedToken.data.refresh_token;
+			console.log(recivedToken.data);
+
+			// getting user's discord data
+			if (recivedToken.data.expires_in) {
+				const userData = await axios.get(
+					'https://discord.com/api/v10/users/@me',
+					{
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: `Bearer ${recivedToken.data.access_token}`,
+						},
+					},
+				);
+				console.log(userData.data);
+				if (userData.data.email) {
+					data.id = userData.data.id;
+					data.name = userData.data.username;
+					data.isEmailPresent = true;
+					data.email = userData.data.email;
+				} else {
+					data.id = userData.data.id;
+					data.name = userData.data.username;
+					data.isEmailPresent = false;
+					problemArray.push('No Email Attached to github Retry!');
+				}
 			} else {
-				data._id = userData.data.id;
-				data.name = userData.data.username;
-				data.isEmailPresent = false;
-				problemArray.push('No Email Attached to github Retry!');
+				console.log('Skipped User Data due to token absence!');
+				flag++;
+			}
+
+			// getting user's github data
+			if (recivedToken.data.expires_in) {
+				const gitData = await axios.get(
+					'https://discord.com/api/v10/users/@me/connections',
+					{
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: `Bearer ${recivedToken.data.access_token}`,
+						},
+					},
+				);
+				console.log(gitData.data);
+				gitData.data.forEach(
+					(element: {
+						type: string;
+						name: string;
+						id: string;
+						visibility: number;
+						show_activity: boolean;
+						verified: boolean;
+						metadata_visibility: number;
+					}) => {
+						if (element.type === 'github') {
+							data.isGuthub = true;
+							data.github.type = element.type;
+							data.github.name = element.name;
+							data.github.id = element.id;
+							data.github.visibility = element.visibility;
+							data.github.show_activity = element.show_activity;
+							data.github.verified = element.verified;
+							data.github.metadata_visibility = element.metadata_visibility;
+						} else {
+							data.isGuthub = false;
+							problemArray.push('Link Your Github to Discord and Retry');
+						}
+					},
+				);
+			} else {
+				console.log('Skipped Git Data due to token absence!');
+				flag++;
+			}
+
+			response.status(200);
+		} else {
+			console.log('There is no code');
+			flag++;
+			response.status(400);
+		}
+		// sending responce
+		if (flag == 0) {
+			console.log('Data to be sent to MongoDb');
+			console.log(data);
+			const result = await addUsers(data);
+			if (result) {
+				response.sendFile('success.html', { root: './src/server/' });
+			} else {
+				response.sendFile('failure.html', { root: './src/server/' });
 			}
 		} else {
-			console.log('Skipped User Data due to token absence!');
-			flag++;
+			response.sendFile('failure.html', { root: './src/server/' });
 		}
-
-		// getting user's github data
-		if (recivedToken.data.expires_in) {
-			const gitData = await axios.get(
-				'https://discord.com/api/v10/users/@me/connections',
-				{
-					headers: {
-						'Content-Type': 'application/json',
-						Authorization: `Bearer ${recivedToken.data.access_token}`,
-					},
-				},
-			);
-			console.log(gitData.data);
-			gitData.data.forEach(
-				(element: {
-					type: string;
-					name: string;
-					id: string;
-					visibility: number;
-					show_activity: boolean;
-					verified: boolean;
-					metadata_visibility: number;
-				}) => {
-					if (element.type === 'github') {
-						data.isGuthub = true;
-						data.github.type = element.type;
-						data.github.name = element.name;
-						data.github.id = element.id;
-						data.github.visibility = element.visibility;
-						data.github.show_activity = element.show_activity;
-						data.github.verified = element.verified;
-						data.github.metadata_visibility = element.metadata_visibility;
-					} else {
-						data.isGuthub = false;
-						problemArray.push('Link Your Github to Discord and Retry');
-					}
-				},
-			);
-		} else {
-			console.log('Skipped Git Data due to token absence!');
-			flag++;
-		}
-
-		response.status(200);
-	} else {
-		console.log('There is no code');
-		flag++;
-		response.status(400);
-	}
-	// sending responce
-	if (flag == 0) {
-		console.log('Data to be sent to MongoDb');
-		console.log(data);
-		response.sendFile('index.html', { root: './src/server/' });
-	} else {
-		response.sendFile('index.html', { root: './src/server/' });
+	} catch (error) {
+		console.log(error);
+		response.sendFile('failure.html', { root: './src/server/' });
 	}
 });
 
--- src/cmd/Auth.ts
+++ src/cmd/Auth.ts
@@ -1,1 +1,80 @@
 // add program to complete Auth command
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ChatInputCommandInteraction,
+	SlashCommandBuilder,
+} from 'discord.js';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('auth')
+		.setDescription('Authenticates you with github!'),
+	async execute(interaction: ChatInputCommandInteraction) {
+		const target = interaction.options.getUser('target');
+		const reason =
+			interaction.options.getString('reason') ?? 'No reason provided';
+
+		const auth = new ButtonBuilder()
+			.setCustomId('auth')
+			.setLabel('Authenticate me')
+			.setStyle(ButtonStyle.Secondary);
+
+		const cancel = new ButtonBuilder()
+			.setCustomId('cancel')
+			.setLabel('Cancel')
+			.setStyle(ButtonStyle.Danger);
+
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+			auth,
+			cancel,
+		);
+
+		const response = await interaction.reply({
+			content: `Are you sure you want to ban ${target} for reason: ${reason}?`,
+			components: [row],
+		});
+
+		try {
+			const confirmation = await response.awaitMessageComponent({
+				filter: (i) => i.user.id === interaction.user.id,
+				time: 60_000,
+			});
+
+			if (confirmation.customId === 'auth') {
+				// await interaction.guild.members.ban(target);
+				await confirmation.update({
+					content:
+						'[Click here](https://discord.com/oauth2/authorize?client_id=1114904309918867496&redirect_uri=http%3A%2F%2Flocalhost%3A53134&response_type=code&scope=identify%20email%20connections)',
+					components: [],
+				});
+			} else if (confirmation.customId === 'cancel') {
+				await confirmation.update({
+					content: 'Action cancelled ...',
+					components: [],
+				});
+			}
+		} catch (e) {
+			console.log(e);
+			await interaction.editReply({
+				content:
+					'Confirmation not received within 1 minute, cancelling authentication process ...',
+				components: [],
+			});
+		}
+		// temp message hidden from other users
+		// await interaction.reply({content:'maccer!', ephemeral: true });
+
+		// permanant messafe message
+		// await interaction.reply('maccer!'});
+
+		// waiting
+		// await wait(4000);
+
+		// reply to message
+		// await interaction.editReply('maccer!');
+		// await interaction.followUp('Pong again!');
+		// await interaction.deleteReply();
+	},
+};
